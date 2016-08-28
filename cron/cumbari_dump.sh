c3a0b5ba7df203edb524db4936e1a526e776af73
kentbogestam <kent.bogestam@cumbari.com>
2016-08-28 23:08:33 +0200

    Update cumbari_dump.sh

--- cron/cumbari_dump.sh
+++ cron/cumbari_dump.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 # Script name : cumbari_dump.sh
-# Dumps all db in advertise 
+# Dumps all db in advertise
 dbname="cumbari_admin"
-date=`date +%Y-%m-%d`
+date=`date "+%d"`
 
 rm -f /var/cumbari/backup/old/*
 
-mv /var/cumbari/backup/*.bk /var/cumbari/backup/yesterday
-mv /var/cumbari/backup/yesterday /var/cumbari/backup/old
+mv /var/cumbari/backup/*.bk /var/cumbari/backup/old/
 
 if [ -f /var/cumbari/backup/cumbari_admin_dump.sql ]
 then
@@ -33,26 +32,22 @@ fi
 
 
 
-if [ -f /var/cumbari/backup/information_schema.sql ]
-then
-mv /var/cumbari/backup/information_schema.sql /var/cumbari/backup/$date'_information_schema.sql.bk'
-fi
+#if [ -f /var/cumbari/backup/information_schema.sql ]
+#then
+#mv /var/cumbari/backup/information_schema.sql /var/cumbari/backup/$date'_information_schema.sql.bk'
+#fi
 #mysqldump information_schema > /var/cumbari/backup/information_schema.sql
 
 #if [ $? -gt 0 ]; then
 #mailx -s "Erron in dumping information_schema on advertise.cumbari.com" admin@cumbari.com </dev/null
 #fi
 
-#tar -cf /tmp/cumbari_$date'.tar' /var/cumbari
-#gzip -q /tmp/cumbari_$date'.tar'
-
-#if [ $? -gt 0 ]; then
-#mailx -s "Erron in creating replica on advertise.cumbari.com" admin@cumbari.com </dev/null
-#else
-#scp -i /root/cumbariadmin1_eu.pem /tmp/cumbari_$date'.tar.gz' root@market.cumbari.com:/var/cumbari/advertise
-#fi
 
 
 #move backup to Amazon S3
+if [ -f /var/cumbari/backup/cumbari_admin_dump.sql ]
+then
+cp /var/cumbari/backup/cumbari_admin_dump.sql /tmp/$date'_cumbari_admin_dump.sql'
+fi
 
-s3put cumbari-backup/cumbari_admin/$date'_cumbari_admin_dump.sql' /var/cumbari/backup/cumbari_admin_dump.sql 
+aws s3 mv /tmp/$date'_cumbari_admin_dump.sql'  s3://cumbari-backup/cumbari_admin/ --acl public-read
