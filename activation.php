faadf6f634984a2a1af27555e1214547295e5ad5
divyaaxovel <divya.ravish@axovel.in>
2018-04-17 12:01:46 +0530

    test

--- activation.php
+++ activation.php
@@ -601,7 +601,37 @@
                
                ?>
             <form name="register" action="" id="registerform" method="Post">
-               
+               <table width="100%" border="0">
+                  <tr>
+                     <td>&nbsp;</td>
+                  </tr>
+                  <tr>
+                     <td class="redwhitebutton">1 Register</td>
+                  </tr>
+                  <tr>
+                     <td  >&nbsp;</td>
+                  </tr>
+                  <tr>
+                     <td class="redwhitebutton">2 Add Company</td>
+                  </tr>
+                 <!--  <tr>
+                     <td  >&nbsp;</td>
+                  </tr>
+                  <?php if(!$_SESSION['Retailers']) {?>
+                  <tr>
+                     <td class="redwhitebutton">3 Add Offer</td>
+                  </tr>
+                  <? } ?> -->
+                  <tr>
+                     <td  >&nbsp;</td>
+                  </tr>
+                  <tr>
+                     <td class="blackbutton">3 Activate</td>
+                  </tr>
+                  <tr>
+                     <td  >&nbsp;</td>
+                  </tr>
+               </table>
                <input class="text_field_new" type="hidden" name="m" value="noOfferDetails">
                <table BORDER=0  width="100%">
                   <tr>
