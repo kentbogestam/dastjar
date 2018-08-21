ce0a1fec827d790d12fe650ea6bfaecc5c72a2f6
Mayank <mayank.pathak@imaginei.in>
2018-08-21 18:02:29 +0530

    log file url updated

--- classes/billing.php
+++ classes/billing.php
@@ -301,7 +301,7 @@ class Billing{
         // echo BASE_URL . "upload/log" . date("Ymd",$t) . ".txt";
         // die();
 
-        $myfile = fopen(BASE_URL . "/upload/log" . date("Ymd",$t) . ".txt", "a") or die("Unable to open file!");
+        $myfile = fopen("upload/log" . date("Ymd",$t) . ".txt", "a") or die("Unable to open file!");
         $txt = date("Y-m-d",$t) . " - " . $str . "  \n";
         fwrite($myfile, $txt);
         fclose($myfile);
