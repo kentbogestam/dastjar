65331b465964ef9b7889256879458d034c38b859
Mayank <mayank.pathak@imaginei.in>
2018-08-21 18:07:25 +0530

    added try catch
    added try catch

--- classes/billing.php
+++ classes/billing.php
@@ -301,7 +301,15 @@ class Billing{
         // echo BASE_URL . "upload/log" . date("Ymd",$t) . ".txt";
         // die();
 
-        $myfile = fopen("upload/log" . date("Ymd",$t) . ".txt", "a") or die("Unable to open file!");
+        // $myfile = fopen("upload/log" . date("Ymd",$t) . ".txt", "a") or die("Unable to open file!");
+
+        try{
+            $myfile = fopen("upload/log" . date("Ymd",$t) . ".txt", "a");
+        }catch(Exception $ex){
+            echo $ex;
+            die();
+        }
+
         $txt = date("Y-m-d",$t) . " - " . $str . "  \n";
         fwrite($myfile, $txt);
         fclose($myfile);
