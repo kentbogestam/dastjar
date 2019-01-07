26565bad449c0458050086a09978b6bb33876c85
Ajit Sigh <ajit.singh.ampliedtech.com>
2019-01-07 16:31:01 +0530

    Error: 'Add Payment' while adding company, issue was redirect_uri was not defined and dummy code while merged before in response file

--- reDirectStrip.php
+++ reDirectStrip.php
@@ -1,4 +1,4 @@
 <?php
 	include_once('cumbari.php');
-	header("Location: https://connect.stripe.com/oauth/authorize?response_type=code&client_id=" . $stripe_client_id . "&scope=read_write");
+	header("Location: https://connect.stripe.com/oauth/authorize?response_type=code&client_id=" . $stripe_client_id . "&scope=read_write&redirect_uri=".$base_url."reDirectResponse.php");
 ?>
