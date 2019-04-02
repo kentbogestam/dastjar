af3162055984f36bae5c2b491427ada595e2b47d
kentbogestam <root@ip-172-31-44-96.eu-west-1.compute.internal>
2019-04-02 07:06:07 +0000

    Merged master branch

--- config/dbConfig.php
+++ config/dbConfig.php
@@ -12,16 +12,15 @@ define("DATABASE", "Secondphase_dastjar");
 define("USER", "root");
 define("PASSWORD", "alirezakent");*/
 
-define("SERVER", "admin-dev.dastjar.com");
+/*define("SERVER", "admin-dev.dastjar.com");
 define("PORT", "3306");
 define("DATABASE", "dastjar");
 define("USER", "root");
-define("PASSWORD", "Test@321D");
+define("PASSWORD", "Test@321D");*/
 
-/*
-define("SERVER", "localhost");
+define("SERVER", "dastjardb.cdlnsxaqoaic.eu-west-1.rds.amazonaws.com");
 define("PORT", "3306");
 define("DATABASE", "dastjar");
-define("USER", "root");
-define("PASSWORD", "12345"); */
+define("USER", "dastjardb");
+define("PASSWORD", "Alirezakent1");
 ?>
