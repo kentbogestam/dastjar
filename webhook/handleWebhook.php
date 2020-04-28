3e2f18427efe830a0e42b92e09561a15d63aa0ee
Ajit Singh <ajitsingh@Kents-MacBook-Air-2.local>
2020-04-28 12:58:06 +0530

    Email: fix send subs thankyou email on update subs

--- webhook/handleWebhook.php
+++ webhook/handleWebhook.php
@@ -105,7 +105,7 @@ class handleWebhook {
 	 * @param  [type] $event [description]
 	 * @return [type]        [description]
 	 */
-	function handleCustomerSubscriptionUpdated()
+	function handleCustomerSubscriptionUpdated($event = null)
 	{
 		$eventData = $event->data->object;
 
