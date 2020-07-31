36558e16f681d3cd825ef159b2d33ecef1f3c0fb
Vishal Verma <42247880+RedHead-Crazyassguy@users.noreply.github.com>
2020-07-31 15:46:00 +0530

    Buttonpage UI change

--- getButtonStore.php
+++ getButtonStore.php
@@ -30,20 +30,25 @@ include_once("main.php");
     h2{
         color:#7e151c;
     }
+    .spanClass{
+        font-size: 14px;
+        margin-left: 80px;
+        color: #28a745;
+        text-shadow: chocolate !important;
+        text-shadow: 2px 2px 10px currentColor;
+    }
 </style>
 <script language="JavaScript" src="client/js/jsStore.js" type="text/javascript"></script>
 <body>
 <div class="center">
 <form name="editStore form" action="" id="registerform" method="POst">
-    <div style="margin-top:15px;" ><h1><b>GET BUTTON</b></h1></div>
+    <div style="margin-top:15px;" ><h1><b>Get a <b>free order online button</b> for your Business/Restraunt!</b></h1></div>
     <table BORDER=0  width="100%">
         <tr>
             <td>
                 <div>
-                    <h2>Buttons to make online ordering from restraunts web pages and Facebook Business Page available to you customers</h2>
-                    <p>A button to copy for web pages</p>
-                    <p>Get a button for your web page to make ordeing instant</p>
-                    <h4>A Button for Eat-now</h4>
+                    <h2>Having an <b>Order Online Button</b> on your restraunt <b>Web-site</b> or <b>Face book page</b>, gives your customers the opportunity to Order directly from several Media/Channels!</h2>
+                    <h4>Order Online Button for EAT NOW</h4>
                     <div style="width:100%;clear:both;">
                         <div style="float:left;width:40%;">
                             <a href="<?=USER_APP_BASE_URL?>restro-menu-list/<?=$data[0]['store_id'] ?>"><button type="button" class="direct-order-button" style="color: #fff; background-color: #28a745; border: none; padding: 8px 11px; font-size: 0.875rem; border-radius: 4px; font-family: sans-serif; font-weight: 600; box-shadow: 4px 4px 6px gray;">Beställ och betala direkt online</button></a>
@@ -56,8 +61,8 @@ include_once("main.php");
                         </div>
                     </div>
                     <div style="clear:both;">
-                    <p>Copy the button above and paste it into Html code of you web site</p>
-                    <h4>A Button for Eat-later</h4>
+                    <p>Copy the button above and paste it into Html code of you web-site or FaceBook</p>
+                    <h4>Order Online Button For EAT LATER/CATERING</h4>
                     <div style="width:100%;clear:both;">
                         <div style="float:left;width:40%;">
                             <a href="<?=USER_APP_BASE_URL?>iframe/eat-later-datetime/<?=$data[0]['store_id'] ?>"><button type="button" class="direct-order-button" style="color: #fff; background-color: #28a745; border: none; padding: 8px 11px; font-size: 0.875rem; border-radius: 4px; font-family: sans-serif; font-weight: 600; box-shadow: 4px 4px 6px gray;">Beställ & betala direkt online</button></a>
@@ -70,10 +75,10 @@ include_once("main.php");
                         </div>
                     </div>
                     <div style="clear:both;">
-                    <p>Copy the button above and paste it into Html code of you web site</p><br><br>
+                    <p>Copy the button above and paste it into Html code of you web-site or FaceBook</p><br><br>
                     <h2> A Link to make  you Facebook Business page ready for online ordering</h2>
                     <p>Add a button to your Facebook page to make ordering instant</p>
-                    <p><b><a href="https://www.facebook.com/business/learn/lessons/add-a-call-to-action-button-on-facebook" style="color:dodgerblue">Check this Video description on how to add it!</a></b>&nbsp;&nbsp;<img src="lib/grid/images/leftArrow.gif" style="vertical-align: top;width:20px;height:15px"></p>
+                    <p><b><a href="https://www.facebook.com/business/learn/lessons/add-a-call-to-action-button-on-facebook" style="color:dodgerblue">Check this Video description on how to add it!</a></b>&nbsp;&nbsp;<img src="lib/grid/images/leftArrow.gif" style="vertical-align: top;width:20px;height:15px"><span class="spanClass">Add a <b>CALL TO ACTION</b> button in your FaceBook business Page!</span></p>
                     <p>To add a CTA button, start on your Page. Below you page's cover photo, click Edit. You'll see a Preview section at the top that shows what you button look like. Select a button to see how to add it will look. if you want people to make a purchase from your shop, click Shop with you.  Add following link.</p>
                     <button onclick="myFunction('eatNowLink')" class="planeButton" type="button"><b>Eat now - </b> Click to copy the link</button><br>
                     <button onclick="myFunction('eatLaterLink')" class="planeButton" type="button"><b>Eat later - </b> Click to copy the link</button> 
--- webhook/handleWebhook.php
+++ webhook/handleWebhook.php
@@ -263,7 +263,7 @@ class handleWebhook {
                         // Send thank-you email
                         $template = file_get_contents(BASEPATH.'email-templates/subscription-confirmation-email.html');
 
-                        $find = array('{{orgNo}}', '{{userName}}', '{{companyName}}', '{{companyAddress}}', '{{storeName}}', '{{theContent}}', '{{storeId}}, {{USER_APP_BASE_URL}}, {{BASEPATH}}');
+                        $find = array('{{orgNo}}', '{{userName}}', '{{companyName}}', '{{companyAddress}}', '{{storeName}}', '{{theContent}}', '{{storeId}}, {{userAppBaseUrl}}, {{basePath}}');
                         $replace = array($user['orgnr'], $user['userName'], $user['company_name'], $user['companyAddress'], $subsDetail['store_name'], $emailContent, $subsDetail['store_id'], USER_APP_BASE_URL, BASEPATH);
                         $template = str_replace($find, $replace, $template);
                         $subject = 'Thank you for subscription!';
